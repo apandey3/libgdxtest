5d637f5831332fefa6adf3944d73cb8770734714
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2016-09-01 11:26:22 -0700

    unpacking zip
    May be in weird state though

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,38 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	BitmapFont font;
+	//Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		font.setColor(com.badlogic.gdx.graphics.Color.BLUE);
+		//img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.begin();
+		font.draw(batch, "Hello World", 200, 200);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		font.dispose();
+		//img.dispose();
+	}
+}
